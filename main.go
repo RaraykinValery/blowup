a70cc08a9136f65777bd2aca2700494d7cf181dd
Valery Raraykin <vand_er@mail.ru>
2023-11-01 06:56:03 +0300

    Added and used StaticHandler for all routs

--- main.go
+++ main.go
@@ -2,39 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/raraykinvalery/blowup/controllers"
 	"github.com/raraykinvalery/blowup/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	HTMLTpl, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(
-			w,
-			"There was a error parsing the template",
-			http.StatusInternalServerError,
-		)
-		return
-	}
-	HTMLTpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
-}
-
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
+
+	tpl, err := views.Parse("templates/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("templates/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not Found", http.StatusNotFound)
 	})
