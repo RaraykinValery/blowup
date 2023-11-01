86461d728c48e1e8f0202b758ed09561aa2050e0
Valery Raraykin <vand_er@mail.ru>
2023-11-01 21:18:26 +0300

    Made an FAQ page

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/raraykinvalery/blowup/views"
@@ -9,5 +10,30 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! Free trial for 30 days.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "24/7",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer: `Email us -
+			<a href="mailto:support@blowup.com">
+			support@blowup.com </a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
